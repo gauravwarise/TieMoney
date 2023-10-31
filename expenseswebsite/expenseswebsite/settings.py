e160ce2a7752597e51f60a03b1c7116c90ab5ca0
gaurav warise <gauravwarise04@gmail.com>
2023-11-01 00:09:32 +0530

    add userpreference

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses',
-    'authentication'
+    'authentication',
+    'userpreferences'
 ]
 
 MIDDLEWARE = [
